fb0ded15184e25932dd8f70174d7b2d20b2135a0
justin0979 <justinm0979@gmail.com>
2021-11-06 13:05:37 -0500

    add _MCRR layer

--- keyboards/crkbd/keymaps/justin0979/keymap.c
+++ keyboards/crkbd/keymaps/justin0979/keymap.c
@@ -40,7 +40,8 @@ extern uint8_t is_master;
 #define _LLS 4        // line 113
 #define _THIRD 5      // line 120
 #define _MCR 6         // line 136
-#define _NP 7         // line 149
+#define _MCRR 7
+#define _NP 8         // line 149
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
@@ -51,6 +52,7 @@ enum custom_keycodes {
   BACKLIT,
   RGBRST,
   MCR,
+  MCRR,
   NP
 };
 
@@ -138,11 +140,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,--------------------------------------------------------.                    ,-----------------------------------------------------.
       KC_ESC,      KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_BSPC,\
   //|--------+-----------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-     TO(_COLEMAK), KC_LSFT, KC_A,    KC_W,    KC_D,    KC_E,                         KC_M,    KC_N,    KC_E,    KC_I,    KC_O, KC_ENT,\
+      KC_F5,     KC_LSFT,   KC_A,    KC_W,    KC_D,    KC_E,                         KC_M,    KC_N,    KC_E,    KC_I,    KC_O, TO(_COLEMAK),\
   //|--------+-----------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
        KC_Q,       KC_Z,    KC_D,    KC_S,    KC_F,    KC_V,                         KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,\
   //|--------+-----------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LCTL, KC_SPC,  KC_F5,     KC_ENT,  KC_SPC,  RSIDE \
+                                          MO(_MCRR), KC_LCTL,  KC_SPC,     KC_ENT,  KC_SPC,  RSIDE \
+                                      //`--------------------------'  `--------------------------'
+
+  ),
+
+  [_MCRR] = LAYOUT_split_3x6_3( \
+  //,--------------------------------------------------------.                    ,-----------------------------------------------------.
+      KC_ESC,      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                         KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_BSPC,\
+  //|--------+-----------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_F5,     KC_LSFT,   KC_A,    KC_W,    KC_D,    KC_E,                         KC_M,    KC_N,    KC_E,    KC_I,    KC_O, TO(_COLEMAK),\
+  //|--------+-----------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+       KC_Q,       KC_Z,    KC_D,    KC_S,    KC_F,    KC_V,                         KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,\
+  //|--------+-----------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                          KC_LCTL, KC_LCTL,  KC_SPC,     KC_ENT,  KC_SPC,  RSIDE \
                                       //`--------------------------'  `--------------------------'
 
   ),
